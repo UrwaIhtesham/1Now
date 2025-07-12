ea48c83abeea185ffc5d39fc9cffaa5b102a9db8
DESKTOP\Urwa <urwaihtesham@gmail.com>
2025-07-12 08:48:51 -0700

    urwa/login: added comments to better understand in future

--- apps/users/views/login.py
+++ apps/users/views/login.py
@@ -8,24 +8,30 @@ from apps.users.utils.validators import validate_email, validate_password
 from django.core.exceptions import ValidationError as DjangoValidationError
 
 class LoginView(APIView):
+    # makes the endpoint public so anyone can login
     permission_classes = [AllowAny]
 
     def post(self, request):
+        #gets email and password from request
         email = request.data.get('email')
         password = request.data.get('password')
 
+        #validate inputs or throw errors
         try:
             validate_email(email)
             validate_password(password)
         except DjangoValidationError as e:
             return Response({'error': str(e)}, status=status.HTTP_401_UNAUTHORIZED)
         
+        #check user's credentials
         user = authenticate(request, email=email, password=password)
 
+        #creates drf token for user
         if user:
             token, _ = Token.objects.get_or_create(user=user)
             return Response({
                 'token': token.key
             }, status= status.HTTP_200_OK)
+        #invalid login
         else:
             return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
