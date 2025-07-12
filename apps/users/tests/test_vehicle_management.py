307bf4286c5758af32460fbaf74e60156d428c4e
DESKTOP\Urwa <urwaihtesham@gmail.com>
2025-07-12 16:54:54 -0700

    urwa/vehicle_management: added comments in test_vehicle_management.py to better understand in future

--- apps/users/tests/test_vehicle_management.py
+++ apps/users/tests/test_vehicle_management.py
@@ -6,15 +6,20 @@ from apps.users.models.vehicle import Vehicle
 from rest_framework_simplejwt.tokens import RefreshToken
 
 class VehicleManagementTests(APITestCase):
+    #Adding initial user for the setup
     def setUp(self):
+        #adding dummy user
         self.user = CustomUser.objects.create_user(
             email='umar@google.com',
             password = 'abc123!'
         )
+        #generating token
         self.token = self.get_access_token(self.user)
+        #inserting token to header
         self.auth_header = {'HTTP_AUTHORIZATION': f'Bearer {self.token}'}
         self.url = f'/api/vehicles/'
 
+    #function to generate token for the user
     def get_access_token(self, user):
         refresh = RefreshToken.for_user(user)
         return str(refresh.access_token)
@@ -22,19 +27,27 @@ class VehicleManagementTests(APITestCase):
     '''
     FOR VEHICLE REGISTRATION
     '''
+    #valid vehicle registration with correct fields and user
     def test_valid_vehicle_registration(self):
+        #adding dummy vehicle
         data = {
             "make": "Honda",
             "model": "Civic",
             "year": 2023,
             "plate": "ABC-1234"
         }
+        #api endpoint with post method
         response = self.client.post(self.url, data, format='json', **self.auth_header)
+        #matching status code
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        #matching counts of vehicle in test db
         self.assertEqual(Vehicle.objects.count(), 1)
+        #matching user of vehicle with the dummy user
         self.assertEqual(Vehicle.objects.first().user, self.user)
 
+    #invalid vehicle registration with empty fields
     def test_invalid_vehicle_registration(self):
+        #adding dummy vehicle with missing field
         data = {
             "make": "", 
             "model": "Civic",
@@ -42,9 +55,11 @@ class VehicleManagementTests(APITestCase):
             "plate": "ABC-123"
         }
         response = self.client.post(self.url, data, format='json', **self.auth_header)
+        #matching status code (should be 400)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn('make', response.data)
 
+    #invalid vehicle registration with unauthorised user
     def test_unauthorized_vehicle_registration(self):
         data = {
             "make": "Honda",
@@ -58,8 +73,9 @@ class VehicleManagementTests(APITestCase):
     '''
     FOR VEHICLE UPDATION
     '''
-
+    #valid vehicle updation with correct fields and user
     def test_valid_vehicle_update(self):
+        #creating dummy vehicle
         vehicle = Vehicle.objects.create(
             user = self.user,
             make='Toyota',
@@ -68,7 +84,7 @@ class VehicleManagementTests(APITestCase):
             plate='abc-123'
         )
         url = f'/api/vehicles/{vehicle.id}/'
-
+        #adding dummy data for updation
         data= {
             "make": "Honda",
             "model": "City",
@@ -77,10 +93,13 @@ class VehicleManagementTests(APITestCase):
         }
         response = self.client.put(url, data, format='json', **self.auth_header)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        #after updation refresh the db to show changes
         vehicle.refresh_from_db()
+        #manual checking of updated vehicle fields
         self.assertEqual(vehicle.make, "Honda")
         self.assertEqual(vehicle.model, "City")
 
+    #invlaid vehicle updation with empty fields
     def test_invalid_vehicle_update(self):
         vehicle = Vehicle.objects.create(
             user = self.user,
@@ -91,6 +110,7 @@ class VehicleManagementTests(APITestCase):
         )
         url = f'/api/vehicles/{vehicle.id}/'
 
+        #adding dummy data with empty fields
         data= {
             "make": "",
             "model": "City",
@@ -106,6 +126,7 @@ class VehicleManagementTests(APITestCase):
     DELETE THE VEHICLE
     '''
 
+    #valid vehicle deletion with authorised user
     def test_valid_vehicle_delete(self):
         vehicle = Vehicle.objects.create(
             user=self.user,
@@ -115,17 +136,18 @@ class VehicleManagementTests(APITestCase):
             plate='ABC-1234'
         )
         url = f'/api/vehicles/{vehicle.id}/'
-
+        #api endpoint delete vehicles/vehicleid
         response = self.client.delete(url, **self.auth_header)
-
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(Vehicle.objects.count(), 0)
 
+    #invalid vehicle deletion with unauthorised user
     def test_invalid_unauthorised_vehicle_delete(self):
+        #creating dummy user who didn't create the vehicle
         other_user = CustomUser.objects.create_user(email='ali@google.com', password="hello123!")
         other_token = self.get_access_token(other_user)
         other_auth_header = {'HTTP_AUTHORIZATION': f'Bearer {other_token}'}
-
+        #create vehicle with the pre-created user
         vehicle=Vehicle.objects.create(
             user=self.user,
             make='Suzuki',
@@ -141,6 +163,8 @@ class VehicleManagementTests(APITestCase):
     '''
     GET LISTING
     '''
+
+    #valid vehicle listing with correct fields and authorised user
     def test_valid_vehicle_listing(self):
         vehicle=Vehicle.objects.create(
             user=self.user,
