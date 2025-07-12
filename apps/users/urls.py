f1716e63e2588103a9d2deb8a53000886bf2e1cd
DESKTOP\Urwa <urwaihtesham@gmail.com>
2025-07-12 16:13:22 -0700

    urwa/vehicle_management: added the api to delete the vehicle

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from apps.users.views.user_registration import RegisterView
 from apps.users.views.login import LoginView
 from apps.users.views.add_vehicle import VehicleCreateView
-from apps.users.views.vehicle_management import VehicleUpdateView
+from apps.users.views.vehicle_management import VehicleUpdateView, VehicleDeleteView
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('add-vehicle/', VehicleCreateView.as_view(), name='add-vehicle'),
-    path('vehicle-update/<int:id>/', VehicleUpdateView.as_view(), name='update-vehicle')
+    path('vehicle-update/<int:id>/', VehicleUpdateView.as_view(), name='update-vehicle'),
+    path('delete-vehicle/<int:id>/', VehicleDeleteView.as_view(), name='delete-vehicle'),
 ]
--- apps/users/views/vehicle_management.py
+++ apps/users/views/vehicle_management.py
@@ -25,3 +25,18 @@ class VehicleUpdateView(APIView):
             return Response(serializer.data, status=status.HTTP_200_OK)
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class VehicleDeleteView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def delete(self, request, id):
+        #get the vehicle object or return 404 error
+        vehicle = get_object_or_404(Vehicle, id=id)
+
+        #only allowing the owner to delete the vehicle
+        if vehicle.user != request.user:
+            return Response({'error': 'You do not have permission to delete this vehicle'}, status=status.HTTP_403_FORBIDDEN)
+        
+        vehicle.delete()
+        return Response({'message': 'Vehicle deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
