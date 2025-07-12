325e18259a29f9f5b32e5b33f94abb82e051c905
DESKTOP\Urwa <urwaihtesham@gmail.com>
2025-07-12 07:42:11 -0700

    urwa/login: created view for login sending jwt authentication token as a response

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from apps.users.views.user_registration import RegisterView
+from apps.users.views.login import LoginView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-
+    path('login/', LoginView.as_view(), name='login'),
 ]
---
+++ apps/users/views/login.py
@@ -0,0 +1,31 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import AllowAny
+from django.contrib.auth import authenticate
+from apps.users.utils.validators import validate_email, validate_password
+from django.core.exceptions import ValidationError as DjangoValidationError
+
+class LoginView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        email = request.data.get('email')
+        password = request.data.get('password')
+
+        try:
+            validate_email(email)
+            validate_password(password)
+        except DjangoValidationError as e:
+            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        
+        user = authenticate(request, email=email, password=password)
+
+        if user:
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response({
+                'token': token.key
+            }, status= status.HTTP_200_OK)
+        else:
+            return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
